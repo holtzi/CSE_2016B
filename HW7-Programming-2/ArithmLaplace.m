11745de87379ff454d9ffe2a28849c1ba8aff4e4
sababa11 <sababa11@gmail.com>
2016-06-01 20:06:00 +0300

    Fixed inputs

--- HW7-Programming-2/ArithmLaplace.m
+++ HW7-Programming-2/ArithmLaplace.m
@@ -5,4 +5,7 @@ fileID = fopen(input_filename, 'rb');
 data = fread(fileID, 'uint16');
 fclose(fileID);
 bin_data = dec2bin(data, 16);
+bin_data_stream = reshape(bin_data', [], 1);
+bin_data_nums = reshape(bin_data_stream, 2, [])';
+data_input = bin2dec(bin_data_nums);
 end
